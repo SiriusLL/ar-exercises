e9ec4f14288f1bf8f5aade189b0f0e4524b64541
SiriusLL <sirius.love.light@gmail.com>
2021-04-27 02:41:34 +0000

    excercise 7

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
 has_many :employees
+validates :name, length: {minimum: 3}
+validates :annual_revenue, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
+  
 end
+
+# validate :must_have_either_womans_or_mens_apparel
+#   if !womens_apparel && !mens_apparel
+#     errors.add(:)
+# def must_have_either_womans_or_mens_apparel
